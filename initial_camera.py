2470a1a20084d59a529942dd80484d7425782ca2
Dre West <noemail@nowhere.nocom>
2018-07-05 03:51:32 +0000

    taken example images with v1

--- initial_camera.py
+++ initial_camera.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 #initial_camera
 
+import time
 '''
 assuming all goes well, this should be set up to run on the pi.
 
@@ -13,26 +15,32 @@ import numpy as np
 import time
 from appJar import gui
 
-SCREEN = "480x800"
+SCREEN = "fullscreen" #"480x800"
 
 imgs = []
 
 def kp(button):
-	
-	pass
+    app.set
+    pass
 
 def scan(button):
-	#open up webcameras, take a picture from each
+        #open up webcameras, take a picture from each
+        if button == 'return':
+                app.setTabbedFrameSelectedTab('device','scan')
+                return
 
 
-	for x in range(3):
-		cam = cv2.VideoCapture(x)
-		_,img = cam.read()
-		#cv2.imshow('cam {}'.format(x), img)
-		del cam
-		imgs.append(img)
-	
-	app.setTabbedFrameSelectedTab('device','write')
+        t = int(time.time())
+        for x in range(3):
+                cam = cv2.VideoCapture(x)
+                _,img = cam.read()
+                #cv2.imshow('cam {}'.format(x), img)
+                del cam
+                #imgs.append(img)
+                cv2.imwrite('{}_{}.jpg'.format(t,x),img)
+
+        
+        app.setTabbedFrameSelectedTab('device','write')
 
 #start a gui
 
@@ -44,23 +52,34 @@ app.startTab('scan')
 
 app.addLabel('instruction','Place a part in and press scan')
 app.addButton('SCAN',scan)
+app.setButtonBg('SCAN','green')
+app.setButtonPadding('SCAN',[90,60])
 
 app.stopTab()
 
 app.startTab('write')
 
-app.addEntry
-#add keypad entry
-app.setStretch("both")
-
-app.setSticky("nesw")
-app.addEntry('something',colspan=3)
-for idx,v in enumerate([7,8,9,4,5,6,1,2,3]):
-	app.addButton(str(v),kp,int(idx/3),idx%3)
-app.addButton('0',kp,app.getRow(),0,colspan=2)
-app.addButton('Back',kp,app.getRow()-1,2)
-
+app.addButton('return',scan)
+app.setButtonPadding('return',[90,60])
+app.addLabel('change part, then return')
+##add keypad entry
+#app.setStretch("both")
+#
+#app.setSticky("nesw")
+#app.addEntry('something',colspan=3)
+#for idx,v in enumerate([7,8,9,4,5,6,1,2,3]):
+#        app.addButton(str(v),kp,1+int((idx)/3),idx%3)
+#app.addButton('0',kp,app.getRow(),0,colspan=2)
+#app.addButton('Back',kp,app.getRow()-1,2)
+#
 app.stopTab()
 app.stopTabbedFrame()
+
+
+# configuration
+
+#app.getEntryWidget('something').config(font="Times 62")
+#app.setEntryMaxLength('something',4)
+#app.setEntryWidth('something',10)
 
 app.go()
